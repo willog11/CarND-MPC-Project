23f510e5154618200610ea4e902c2774ef9cf3bb
willog11 <willog11@gmail.com>
2017-10-30 13:54:58 +0000

    Tweaking params

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -42,7 +42,7 @@ const int epsi_weight = 3000;
 const int v_weight = 1;
 const int delta_weight = 5;
 const int a_weight = 5;
-const int delta_a_weight = 800;
+const int delta_a_weight = 600;
 const int delta_smooth_weight = 200;
 const int a_smooth_weight = 10;
 
