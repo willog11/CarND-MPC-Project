1b48f6cc45d00e348360c3fe56097898ad910af5
willog11 <willog11@gmail.com>
2017-10-30 13:46:39 +0000

    Tweaking params

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -39,7 +39,7 @@ size_t a_start = delta_start + N - 1;
 // Weights to be applier to each cost function - the higher the value the greater of importance
 const int cte_weight = 3000;
 const int epsi_weight = 3000;
-const int v_weight = 5;
+const int v_weight = 1;
 const int delta_weight = 10;
 const int a_weight = 10;
 const int delta_a_weight = 700;
