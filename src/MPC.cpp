7153a3ae73af7241ab4d7ce8ceae26095272303f
willog11 <willog11@gmail.com>
2017-10-30 13:01:19 +0000

    Tweaking params

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -22,7 +22,7 @@ double dt = 0.1;
 const double Lf = 2.67;
 
 // Reference velocity
-const double v_ref = 75;
+const double v_ref = 70;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
@@ -43,7 +43,7 @@ const int v_weight = 1;
 const int delta_weight = 5;
 const int a_weight = 5;
 const int delta_a_weight = 650;
-const int delta_smooth_weight = 100;
+const int delta_smooth_weight = 200;
 const int a_smooth_weight = 10;
 
 class FG_eval {
