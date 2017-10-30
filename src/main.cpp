98d4a839540e365b8a71a72ba752230bf2b4324a
willog11 <willog11@gmail.com>
2017-10-30 11:38:41 +0000

    Normalizing steering

--- src/main.cpp
+++ src/main.cpp
@@ -154,7 +154,7 @@ int main() {
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = result[0];
+          msgJson["steering_angle"] = result[0] / deg2rad(25);
           msgJson["throttle"] = result[1];
 
           //Display the MPC predicted trajectory 
