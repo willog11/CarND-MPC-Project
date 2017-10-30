3920db7fbb5b2f0371ada89e2bdc30e769529aaf
willog11 <willog11@gmail.com>
2017-10-30 16:03:32 +0000

    Tweaking params

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -38,15 +38,15 @@ size_t a_start = delta_start + N - 1;
 
 // Weights to be applier to each cost function - the higher the value the greater of importance
 
-//3500 slow but stable, 2000 near miss
+//2250 slow but stable, 2000 near miss
 const int cte_weight = 2250;
 const int epsi_weight = 2250;
 const int v_weight = 1;
 const int delta_weight = 5;
 const int a_weight = 5;
-const int delta_a_weight = 600;
+const int delta_a_weight = 650;
 const int delta_smooth_weight = 200;
-const int a_smooth_weight = 10;
+const int a_smooth_weight = 15;
 
 class FG_eval {
  public:
