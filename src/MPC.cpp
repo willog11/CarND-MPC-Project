cd4daea885d1169cd1eef596df89d1837fa8f0f8
wogrady <willog11@gmail.com>
2017-10-31 11:45:36 +0000

    Increasing max speed

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -22,7 +22,7 @@ double dt = 0.075;
 const double Lf = 2.67;
 
 // Reference velocity
-const double v_ref = 80;
+const double v_ref = 100;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
