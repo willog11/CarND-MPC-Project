8dc0ac7cd754af1a05615369ee801cb8ade1c748
wogrady <willog11@gmail.com>
2017-10-31 09:37:25 +0000

    Tweaking params

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -22,7 +22,7 @@ double dt = 0.1;
 const double Lf = 2.67;
 
 // Reference velocity
-const double v_ref = 60;
+const double v_ref = 50;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
@@ -39,14 +39,14 @@ size_t a_start = delta_start + N - 1;
 // Weights to be applier to each cost function - the higher the value the greater of importance
 
 //2250 slow but stable, 2000 near miss
-const int cte_weight = 5000;
-const int epsi_weight = 5000;
+const int cte_weight = 1000;
+const int epsi_weight = 1000;
 const int v_weight = 1;
 const int delta_weight = 50;
 const int a_weight = 50;
 //const int delta_a_weight = 500;
-const int delta_smooth_weight = 5000;
-const int a_smooth_weight = 100;
+const int delta_smooth_weight = 25000;
+const int a_smooth_weight = 5000;
 
 class FG_eval {
  public:
